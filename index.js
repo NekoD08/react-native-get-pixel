cbbdbb230db7b66ef9ac41084fb281e93b68d8c6
Ricardo Blancas <43024565+NekoD08@users.noreply.github.com>
2019-08-29 13:06:59 -0500

    Add files via upload
    Export funtion to get rank of a pixels

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {
 
 export function getPixelRGBA(imageName, x, y) {
   return new Promise((resolve, reject) => {
-      NativeModules.RNPixelColor.getPixelRGBAofImage(imageName, x, y, (err, color) => {        
+      NativeModules.RNPixelColor.getPixelRGBAofImage(imageName, x, y, (err, color) => {
         if (err) {
           return reject(err);
         }
@@ -15,7 +15,7 @@ export function getPixelRGBA(imageName, x, y) {
 
 export function getPixelRGBAPolar(imageName, angle, radius) {
   return new Promise((resolve, reject) => {
-      NativeModules.RNPixelColor.getPixelRGBAPolarOfImage(imageName, angle, radius, (err, color) => {        
+      NativeModules.RNPixelColor.getPixelRGBAPolarOfImage(imageName, angle, radius, (err, color) => {
         if (err) {
           return reject(err);
         }
@@ -26,11 +26,44 @@ export function getPixelRGBAPolar(imageName, angle, radius) {
 
 export function findAngleOfNearestColor(imageName, minAngle, maxAngle, radius, targetColor) {
   return new Promise((resolve, reject) => {
-      NativeModules.RNPixelColor.findAngleOfNearestColor(imageName, minAngle, maxAngle, radius, targetColor, (err, angle) => {        
+      NativeModules.RNPixelColor.findAngleOfNearestColor(imageName, minAngle, maxAngle, radius, targetColor, (err, angle) => {
         if (err) {
           return reject(err);
         }
         resolve(angle);
       });
+  });
+}
+
+export function getPixelRank(imageName, x0, y0, x1, y1) {
+  return new Promise((resolve, reject) => {
+      NativeModules.RNPixelColor.getPixelRankofImage(imageName, x0, y0, x1, y1, (err, colorArr) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(colorArr);
+      });
+  });
+}
+
+export function getPixelRankHSV(imageName, x0, y0, x1, y1) {
+  return new Promise((resolve, reject) => {
+    NativeModules.RNPixelColor.getPixelRankHSVofImage(imageName, x0, y0, x1, y1, (err, colorArrHSV) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(colorArrHSV);
+    });
+  });
+}
+
+export function getPixelHSV(imageName, x, y) {
+  return new Promise((resolve, reject) => {
+    NativeModules.RNPixelColor.getPixelHSVofImage(imageName, x, y, (err, pixelHSV) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(pixelHSV);
+    });
   });
 }
